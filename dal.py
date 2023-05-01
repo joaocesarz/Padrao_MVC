4ce0b5a360126d5b71f25e78d896f6706a4bcbe5
joaocesarz <mercadinhodoleo95@gmail.com>
2023-04-30 21:19:48 -0300

    Alterando ler dados em arquivo txt

--- dal.py
+++ dal.py
@@ -1,31 +1,37 @@
 # Dal seria que faz o acesso ao banco de dados, qualquer tipo de armazenamento persistente
 
-from model import Produto
+from model import Produtos
 
 
 class ProdutosDal:
-
-    @classmethod             # método de uma classe que pode ser chamado diretamente, sem ser instânciado. ele  recebe o parametro cls
-    def salvar(cls, produto: Produto):
+    @classmethod
+    def salvar(cls, produto: Produtos):
         with open('produtos.txt', 'a') as arq:
-            arq.writelines(produto.nome + ' ' + produto.quantidade + ' ' + produto.validade)
-            arq.writelines('\n')
-
-
+            arq.writelines(
+                produto.nome
+                + '|'
+                + produto.quantidade
+                + '|'
+                + produto.validade
+                + '\n'
+            )
 
     @classmethod
     def ler(cls):
         with open('produtos.txt', 'r') as arq:
-            cls.nome = arq.readlines()
+            cls.nome_p = arq.readlines()
 
-        
         produtos = []
 
-
-        if len(cls.nome) > 0:
-            for i in cls.nome:
-                produtos.append(i)
+        if len(cls.nome_p) > 0:
+            for i in cls.nome_p:
+                i = i.split('|')
+                produtos.append(Produtos(i[0], i[1], i[2]))
 
         return produtos
 
-        
+    @classmethod
+    def listar_estoque(cls):
+        arquivo = open('produtos.txt', 'r')
+        listat_est = arquivo.readlines()
+        return listat_est
